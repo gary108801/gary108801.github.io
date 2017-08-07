96be87d1f82f8e88b2eedfb813ffa08b6a7d6bb5
gary108801 <gary108801@gmail.com>
2017-08-08 01:02:37 +0800

    hw2

---
+++ hw2/js/index.js
@@ -0,0 +1,51 @@
+$('#insert').on('click', function() {
+    var data = {
+        name: $('#InputProductName').val(),
+        price: +$('#InputProductPrice').val(),
+        count: +$('#InputProductCount').val(),
+        image: $('#InputProductImage').val()
+    }
+    $.post("http://js2017-hw2.kchen.club/insert", data, function(response) {
+        if (response) {
+            if (response.result) {
+                $('#message').text('新增成功');
+            } else {
+                $('#message').text('新增失敗 (' + response.data + ')');
+            }
+        } else {
+            $('#message').text('新增失敗');
+        }
+        $('#dialog').modal('show');
+        console.log(response);
+    }, "json");
+})
+
+
+
+$('#query').on('click', function() {
+    $.get("http://js2017-hw2.kchen.club/query", function(response) {
+            if (response) {
+                if (response.result) {
+                    {
+                        "_id": "",
+                        "name": "",
+                        "price": "",
+                        "count": "",
+                        "image": "",
+                        "__v": 0
+                    }
+                    $('#dialog').modal('show');
+                }
+
+
+            } else {
+                $('#message').text('查詢失敗 (' + response.data + ')');
+                $('#dialog').modal('show');
+            }
+        } else {
+            $('#message').text('查詢失敗');
+            $('#dialog').modal('show');
+        }
+        console.log(response);
+    }, "json");
+})
